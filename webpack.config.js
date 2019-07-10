695724137c74bbee1da8531c5929f88ab89f7b45
Max Gilbert <max.gilbert@bluewren.co.uk>
2019-07-10 09:15:51 +0100

    Added getFormDataObject() method for file uploads

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,30 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-    entry: path.join(__dirname, "src/index.js"),
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-object-rest-spread']
-                    }
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".js", ".jsx"],
-        alias: {
-            react: path.resolve(path.join(__dirname, './node_modules/react')),
+  entry: path.resolve(path.join(__dirname, "src/index.js")),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"]
+          }
         }
-    },
-    externals: {
-        'react': 'commonjs react' 
-    }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  externals: {
+    react: "commonjs react"
+  }
 };
